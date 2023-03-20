4ae5132f9cedf017b674be93c7cd9cf2881e8a09
mkkkpln <mika.kopalina@gmail.com>
2023-03-20 15:28:15 +0300

    Initial commit

--- src/main/java/commands/PrintFieldDescendingMood.java
+++ src/main/java/commands/PrintFieldDescendingMood.java
@@ -1,16 +1,33 @@
 package commands;
 
+import data.HumanBeing;
+import managers.CollectionManager;
 import utils.Environment;
+import utils.WrongScriptException;
 
 public class PrintFieldDescendingMood implements ICommand {
+    private CollectionManager collectionManager;
+    private HumanBeing human;
     @Override
-    public void execute(Environment environment, String message) {
-
+    public void execute(Environment environment, String message) throws WrongScriptException {
+        collectionManager = environment.getCollectionManager();
+        try{
+            for (HumanBeing people : collectionManager.getPeople() ) {
+                if (people.getMood().compareTo(people.getMood(),human.getMood())){
+                    System.out.println(people.toString());
+                }
+            }
+        }catch (Exception e){
+            if(environment.getPointer()>0){
+                throw new WrongScriptException();
+            }
+            System.out.println("Исключение!");
+        }
     }
 
     @Override
     public String getName() {
-        return "print_field_descending_mood";
+        return "printFieldDescendingMood";
     }
 
     @Override
