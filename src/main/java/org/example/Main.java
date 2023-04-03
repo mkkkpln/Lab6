1e624dcf3751a0c29f0b0abb00c03edd7af42d7f
mkkkpln <mika.kopalina@gmail.com>
2023-04-03 21:31:42 +0300

    "Commit"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,28 +16,30 @@ public class Main {
         CollectionManager manager;
 
         //Загрузим коллекцию из файла
-        String link = args[0];
+        if (args.length > 0) {
+            String link = args[0];
 
-        try {
-            manager = XmlUtil.XMLParser(link);
+            try {
+                manager = XmlUtil.XMLParser(link);
 
-            //Генерируем все необходимое для паттерна "Command"
-            ArrayList<String> history = new ArrayList<>();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            PrintStream writer = new PrintStream(System.out);
-            ICommand[] commands = new ICommand[]{new Help(), new Info(), new Exit(), new Clear(), new Show(), new Insert(), new SumOfImpactSpeed(), new RemoveKey(), new RemoveGreater(), new ReplaceIfLower(), new ExecuteScript(), new PrintFieldDescendingMood(), new Save(), new UpdateID(), new PrintAscending(), new RemoveLowerKey()};
-            Environment environment = new Environment(manager, reader, writer, history, commands);
-            Invoker invoker = new Invoker(environment, commands);
+                //Генерируем все необходимое для паттерна "Command"
+                ArrayList<String> history = new ArrayList<>();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                PrintStream writer = new PrintStream(System.out);
+                ICommand[] commands = new ICommand[]{new Help(), new Info(), new Exit(), new Clear(), new Show(), new Insert(), new SumOfImpactSpeed(), new RemoveKey(), new RemoveGreater(), new ReplaceIfLower(), new ExecuteScript(), new PrintFieldDescendingMood(), new Save(), new UpdateID(), new PrintAscending(), new RemoveLowerKey()};
+                Environment environment = new Environment(manager, reader, writer, history, commands);
+                Invoker invoker = new Invoker(environment, commands);
 
-            //запускаем программу
-            IO.commandReader(environment, invoker);
+                //запускаем программу
+                IO.commandReader(environment, invoker);
 
-        } catch (ParserConfigurationException | IOException e) {
-            System.err.println("ParserConfigurationException or IOException caught...");
-            e.printStackTrace();
-        } catch (SAXException e) {
-            System.err.println("SAXException caught...");
-            e.printStackTrace();
+            } catch (ParserConfigurationException | IOException e) {
+                System.err.println("ParserConfigurationException or IOException caught...");
+                e.printStackTrace();
+            } catch (SAXException e) {
+                System.err.println("SAXException caught...");
+                e.printStackTrace();
+            }
         }
 
     }
