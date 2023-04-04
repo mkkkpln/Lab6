7780cf0162f1997377824fce5a394220232c7026
mkkkpln <mika.kopalina@gmail.com>
2023-04-04 11:18:11 +0300

    "Commit"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,34 +13,38 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        CollectionManager manager;
+        CollectionManager manager = new CollectionManager();
+
 
         //Загрузим коллекцию из файла
         if (args.length > 0) {
             String link = args[0];
+            File f = new File(link);
 
-            try {
-                manager = XmlUtil.XMLParser(link);
+            if (f.exists() && !f.isDirectory()) {
 
-                //Генерируем все необходимое для паттерна "Command"
-                ArrayList<String> history = new ArrayList<>();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                PrintStream writer = new PrintStream(System.out);
-                ICommand[] commands = new ICommand[]{new Help(), new Info(), new Exit(), new Clear(), new Show(), new Insert(), new SumOfImpactSpeed(), new RemoveKey(), new RemoveGreater(), new ReplaceIfLower(), new ExecuteScript(), new PrintFieldDescendingMood(), new Save(), new UpdateID(), new PrintAscending(), new RemoveLowerKey()};
-                Environment environment = new Environment(manager, reader, writer, history, commands);
-                Invoker invoker = new Invoker(environment, commands);
+                try {
+                    manager = XmlUtil.XMLParser(link);
 
-                //запускаем программу
-                IO.commandReader(environment, invoker);
+                    //Генерируем все необходимое для паттерна "Command"
+                    ArrayList<String> history = new ArrayList<>();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                    PrintStream writer = new PrintStream(System.out);
+                    ICommand[] commands = new ICommand[]{new Help(), new Info(), new Exit(), new Clear(), new Show(), new Insert(), new SumOfImpactSpeed(), new RemoveKey(), new RemoveGreater(), new ReplaceIfLower(), new ExecuteScript(), new PrintFieldDescendingMood(), new Save(), new UpdateID(), new PrintAscending(), new RemoveLowerKey()};
+                    Environment environment = new Environment(manager, reader, writer, history, commands);
+                    Invoker invoker = new Invoker(environment, commands);
 
-            } catch (ParserConfigurationException | IOException e) {
-                System.err.println("ParserConfigurationException or IOException caught...");
-                e.printStackTrace();
-            } catch (SAXException e) {
-                System.err.println("SAXException caught...");
-                e.printStackTrace();
+                    //запускаем программу
+                    IO.commandReader(environment, invoker);
+
+                } catch (ParserConfigurationException | IOException e) {
+                    System.err.println("ParserConfigurationException or IOException caught...");
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    System.err.println("SAXException caught...");
+                    e.printStackTrace();
+                }
             }
         }
-
     }
 }
